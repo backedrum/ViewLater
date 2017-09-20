b7e637cc359474d622bac4ee2bf80436cc8f0b00
backedrum <andrey.zablodskiy@gmail.com>
2017-09-20 23:50:13 +0200

    Add: Show screenshot of the pinned site.

--- app.go
+++ app.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2017 Andrii Zablodskyi (andrey.zablodskiy@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -9,9 +24,10 @@ import (
 )
 
 type Link struct {
-	Id    int    `json:"id"`
-	Url   string `json:"url"`
-	Title string `json:"title"`
+	Id         int    `json:"id"`
+	Url        string `json:"url"`
+	Title      string `json:"title"`
+	Screenshot string `json:"screenshot"`
 }
 
 type LinksList []Link
@@ -19,6 +35,8 @@ type LinksList []Link
 var storage = js.Global.Get("localStorage")
 
 var urls = LinksList{}
+
+var screenshot = ""
 
 func main() {
 
@@ -40,23 +58,26 @@ func main() {
 	for _, link := range urls {
 		row := d.CreateElement("tr").(*dom.HTMLTableRowElement)
 		urlColumn := d.CreateElement("td").(*dom.HTMLTableCellElement)
+		addRemoveButton(d, table, row, link.Id)
 		row.AppendChild(urlColumn)
 
 		urlColumn.SetAttribute("id", strconv.Itoa(link.Id))
 		urlColumn.SetInnerHTML("<a href=\"" + link.Url + "\">" + link.Title + "</a>")
 
-		addRemoveButton(d, table, row, link.Id)
+		addScreenshot(d, row, link.Screenshot)
 		table.AppendChild(row)
 	}
 
 	add := d.GetElementByID("addButton").(*dom.HTMLInputElement)
 	add.Call("addEventListener", "click", func(event *js.Object) {
 		row := d.CreateElement("tr").(*dom.HTMLTableRowElement)
-		urlColumn := d.CreateElement("td").(*dom.HTMLTableCellElement)
-		row.AppendChild(urlColumn)
 
 		c.Windows.GetCurrent(chrome.Object{}, func(window chrome.Window) {
 			id := window.Id
+
+			c.Tabs.CaptureVisibleTab(id, nil, func(dataUrl string) {
+				screenshot = dataUrl
+			})
 
 			c.Tabs.GetSelected(id, func(tab chrome.Tab) {
 				currentUrl := tab.Url
@@ -66,12 +87,17 @@ func main() {
 					nextId = urls[len(urls)-1].Id + 1
 				}
 
+				addRemoveButton(d, table, row, nextId)
+
+				urlColumn := d.CreateElement("td").(*dom.HTMLTableCellElement)
 				urlColumn.SetAttribute("id", strconv.Itoa(nextId))
 				urlColumn.SetInnerHTML("<a href=\"" + currentUrl + "\">" + tab.Title + "</a>")
-				addRemoveButton(d, table, row, nextId)
+				row.AppendChild(urlColumn)
+
+				addScreenshot(d, row, screenshot)
 				table.AppendChild(row)
 
-				urls = append(urls, Link{Id: nextId, Url: currentUrl, Title: tab.Title})
+				urls = append(urls, Link{Id: nextId, Url: currentUrl, Title: tab.Title, Screenshot: screenshot})
 				marshalUrlsToStorage()
 			})
 
@@ -79,7 +105,26 @@ func main() {
 	})
 }
 
+func addScreenshot(d dom.Document, tableRow *dom.HTMLTableRowElement, screenshot string) {
+	screenshotColumn := d.CreateElement("td").(*dom.HTMLTableCellElement)
+
+	div := d.CreateElement("div").(*dom.HTMLDivElement)
+	div.Style().Set("height", "90px")
+	div.Style().Set("width", "240px")
+
+	img := d.CreateElement("img").(*dom.HTMLImageElement)
+	img.SetAttribute("src", screenshot)
+	img.Style().Set("max-height", "100%")
+	img.Style().Set("max-width", "100%")
+	div.AppendChild(img)
+
+	screenshotColumn.AppendChild(div)
+	tableRow.AppendChild(screenshotColumn)
+}
+
 func addRemoveButton(d dom.Document, table dom.Element, tableRow *dom.HTMLTableRowElement, urlId int) {
+	removeButtonColumn := d.CreateElement("td").(*dom.HTMLTableCellElement)
+
 	remove := d.CreateElement("input").(*dom.HTMLInputElement)
 	remove.SetAttribute("type", "button")
 	remove.SetAttribute("value", "X")
@@ -90,9 +135,8 @@ func addRemoveButton(d dom.Document, table dom.Element, tableRow *dom.HTMLTableR
 		marshalUrlsToStorage()
 		table.RemoveChild(tableRow)
 	})
-	tableRow.AppendChild(remove)
-
-	return
+	removeButtonColumn.AppendChild(remove)
+	tableRow.AppendChild(removeButtonColumn)
 }
 
 func removeLink(id int) {
